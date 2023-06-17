667ca81ead11caf0f676a86d4b123673c94237ed
[Ghias Naser] <[ghiasnaser@gmail.com]>
2023-06-17 15:01:48 -0700

    set the relationship between tables in the index.js

--- models/index.js
+++ models/index.js
@@ -4,13 +4,35 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+
+// Products belongsTo Category
+Product.belongsTo(Category,{
+  foreignKey:'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag,{
+  through:{
+    model:ProductTag,
+    unique:false,
+  },
+  as: 'Tags'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,{
+  through:{
+    model:ProductTag,
+    unique:false,
+  },
+  as:'Products'
+});
 
 module.exports = {
   Product,
