e3d73f3b267618a5c709645398a334ed8ffcc669
[Ghias Naser] <[ghiasnaser@gmail.com]>
2023-06-17 16:00:41 -0700

    set the get route api/categories/:id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -14,13 +14,25 @@ router.get('/', async(req, res) => {
   }catch(error){
     console.error('Error fetching categories:', error);
     res.status(500).json({ error: 'Error fetching categories' });
-  }
+  };
 });
 
 router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
- 
+ const category_id=req.params.id;
+ try{
+  const category = await Category.findByPk(category_id,{
+    include:[Product],
+  });
+  if (!category){
+    return res.status(404).json({error:'Category not found'});
+  }
+  res.json(category);
+ }catch(error){
+  console.error('Error fetching category:', error);
+    res.status(500).json({ error: 'Error fetching category' });
+ };
 });
 
 router.post('/', (req, res) => {
