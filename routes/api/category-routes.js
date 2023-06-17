98c167d30415a6a860341b1544291be07994557d
[Ghias Naser] <[ghiasnaser@gmail.com]>
2023-06-17 16:09:37 -0700

    set the post route api/categories to add category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -28,15 +28,22 @@ router.get('/:id', async (req, res) => {
   if (!category){
     return res.status(404).json({error:'Category not found'});
   }
-  res.json(category);
+  res.status(200).json(category);
  }catch(error){
   console.error('Error fetching category:', error);
-    res.status(500).json({ error: 'Error fetching category' });
+  res.status(500).json({ error: 'Error fetching category' });
  };
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   // create a new category
+  try{
+    const categoryData=await Category.create(req.body);
+    res.status(200).json(categoryData);
+  }catch(error){
+    console.error('Error creating category:', error);
+    res.status(500).json({ error: 'Error creating category' });
+  };
 });
 
 router.put('/:id', (req, res) => {
