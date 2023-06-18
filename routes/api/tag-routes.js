828a06f6cad84af88ce688f9f3108be272eab5d1
[Ghias Naser] <[ghiasnaser@gmail.com]>
2023-06-17 17:24:13 -0700

    set the get route api/tags/:id to get a tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -17,9 +17,22 @@ router.get('/', async (req, res) => {
   };
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  const tag_id=req.params.id;
+ try{
+  const tag = await Tag.findByPk(tag_id,{
+    include:[{model:Product,as:'Products'}],
+  });
+  if (!tag){
+    return res.status(404).json({error:'tag not found'});
+  }
+  res.status(200).json(tag);
+ }catch(error){
+  console.error('Error fetching tag:', error);
+  res.status(500).json({ error: 'Error fetching tag' });
+ };
 });
 
 router.post('/', (req, res) => {
